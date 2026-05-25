5d789f5221fa7c6954ff630ae22c356805c0c284
Sai Krishna Mateti <104357035+saikrishna6800@users.noreply.github.com>
2026-05-25 17:30:49 +0530

    feat: initialize server entry point with database connection, routing, and Swagger documentation

--- src/server.js
+++ src/server.js
@@ -10,12 +10,22 @@ const swaggerSpec = require("./config/swagger");
 // async function run() {
 //   try {
 const options = {
+  explorer: true,
   // customCssUrl:
   //   "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-material.css",
   customSiteTitle: "PetBuddy API Documentation",
 };
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    explorer: true,
+    // customCssUrl:
+    //   "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-material.css",
+    customSiteTitle: "PetBuddy API Documentation",
+  }),
+);
 app.get("/swagger.json", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(swaggerSpec);

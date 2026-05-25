941d5bd3a9e199769cfe8e97fe1fafb0725bacd5
Sai Krishna Mateti <104357035+saikrishna6800@users.noreply.github.com>
2026-05-25 16:38:23 +0530

    feat: implement swagger documentation configuration and add required dependencies

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,4 +1,5 @@
 const swaggerJsdoc = require("swagger-jsdoc");
+const path = require("path");
 
 const options = {
   definition: {
@@ -20,7 +21,7 @@ const options = {
     ],
   },
   // Paths to files containing OpenAPI definitions
-  apis: ["./src/routes/*.js", "./src/controllers/*.js"],
+  apis: [path.join(__dirname, "../routes/*.js")],
 };
 
 const swaggerSpec = swaggerJsdoc(options);

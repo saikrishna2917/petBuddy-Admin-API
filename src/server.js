ab9baac7eec2336dd112628fcdd3b18fa502cd94
Sai Krishna Mateti <104357035+saikrishna6800@users.noreply.github.com>
2026-05-28 16:14:28 +0530

    feat: implement server initialization logic and express configuration in server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,65 @@
+// require("dotenv").config();
+// const app = require("./utils/express");
+// const connectDB = require("./config/db");
+// const logger = require("./utils/logger");
+// const routes = require("./routes");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerSpec = require("./config/swagger");
+// const { authorization } = require("./middleware/authMiddleware");
+
+// // Start the server
+// async function run() {
+//   try {
+//     await connectDB();
+//     app.use(authorization);
+//     app.use(
+//       "/api-docs",
+//       swaggerUi.serve,
+//       swaggerUi.setup(swaggerSpec, {
+//         explorer: true,
+//         customCssUrl:
+//           "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-material.css",
+//         customSiteTitle: "PetBuddy API Documentation",
+//       }),
+//     );
+//     app.get("/swagger.json", (req, res) => {
+//       res.setHeader("Content-Type", "application/json");
+//       res.send(swaggerSpec);
+//     });
+//     app.use(routes);
+
+//     app.get("/", (req, res) => {
+//       res.status(200).json({
+//         success: true,
+//         message: "PetBuddy API Running Successfully",
+//       });
+//     });
+//     if (process.env.NODE_ENV.toLocaleLowerCase() !== "production") {
+//       app.listen(process.env.PORT, process.env.HOST, function (err) {
+//         if (err) {
+//           logger.error(`Failed to start server: ${err}`);
+//         } else {
+//           logger.info(
+//             `PetBuddy admin portal is running on http://${process.env.HOST}:${process.env.PORT}`,
+//           );
+//         }
+//       });
+//     }
+//   } catch (err) {
+//     logger.error(`Server Error: ${err}`);
+//     throw new Error(err);
+//   }
+// }
+
+// module.exports = app;
+
+// if (require.main === module) {
+//   run();
+// }
+
+
 require("dotenv").config();
+
 const app = require("./utils/express");
 const connectDB = require("./config/db");
 const logger = require("./utils/logger");
@@ -7,11 +68,16 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 const { authorization } = require("./middleware/authMiddleware");
 
-// Start the server
-async function run() {
+async function initializeApp() {
   try {
+    console.log("Connecting MongoDB...");
+
     await connectDB();
+
+    console.log("MongoDB Connected Successfully");
+
     app.use(authorization);
+
     app.use(
       "/api-docs",
       swaggerUi.serve,
@@ -22,10 +88,12 @@ async function run() {
         customSiteTitle: "PetBuddy API Documentation",
       }),
     );
+
     app.get("/swagger.json", (req, res) => {
       res.setHeader("Content-Type", "application/json");
       res.send(swaggerSpec);
     });
+
     app.use(routes);
 
     app.get("/", (req, res) => {
@@ -34,25 +102,19 @@ async function run() {
         message: "PetBuddy API Running Successfully",
       });
     });
-    if (process.env.NODE_ENV.toLocaleLowerCase() !== "production") {
-      app.listen(process.env.PORT, process.env.HOST, function (err) {
-        if (err) {
-          logger.error(`Failed to start server: ${err}`);
-        } else {
-          logger.info(
-            `PetBuddy admin portal is running on http://${process.env.HOST}:${process.env.PORT}`,
-          );
-        }
+
+    if (process.env.NODE_ENV !== "production") {
+      app.listen(process.env.PORT, () => {
+        logger.info(`Server running on port ${process.env.PORT}`);
       });
     }
   } catch (err) {
+    console.log("Server Initialization Error:", err);
+
     logger.error(`Server Error: ${err}`);
-    throw new Error(err);
   }
 }
 
-module.exports = app;
+initializeApp();
 
-if (require.main === module) {
-  run();
-}
+module.exports = app;
